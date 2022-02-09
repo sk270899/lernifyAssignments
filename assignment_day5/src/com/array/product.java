d6c96a3715ee39730b7187f1b894375a85f5a102
Sahil Khandelwal <sahilkhandelwal101@gmail.com>
2022-02-10 01:27:38 +0530

    added assignments for day 5

---
+++ assignment_day5/src/com/array/product.java
@@ -0,0 +1,12 @@
+//Que 2 session 2
+
+package com.array;
+
+public class product {
+	product() {
+		System.out.println("product class constructor called");
+	}
+	public static void main(String[] args) {
+		product p = new product();
+	}
+}
