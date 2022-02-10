31fb74f7596000d4020a76e8d0a62513fe5dbff6
Sahil Khandelwal <sahilkhandelwal101@gmail.com>
2022-02-10 17:59:41 +0530

    assignment added

--- Day10/assignment_day10/src/com/SearchingSorting/SelectionSort.java
+++ Day10/assignment_day10/src/com/SearchingSorting/SelectionSort.java
@@ -1,5 +1,30 @@
 package com.SearchingSorting;
 
 public class SelectionSort {
-
+	
+	static void selectionSort(int []arr) {
+		for(int i=0;i<arr.length-1;i++) {
+			int min = i;
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i]>arr[j])
+					min = j;
+			}
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+	}
+	public static void main(String[] args) {
+		int []arr = {12,45,10,32,7,37,99};
+		System.out.println("elements before sorting : ");
+		for(int x:arr) {
+			System.out.print(x+" ");
+		}
+		System.out.println("");
+		selectionSort(arr);
+		System.out.println("elements after sorting : ");
+		for(int x:arr) {
+			System.out.print(x+" ");
+		}
+	}
 }
