7e09159c6096fbd1ff70f3c6509c28e435d839f6
Sahil Khandelwal <sahilkhandelwal101@gmail.com>
2022-02-15 11:39:43 +0530

    added assignment

--- Day11/src/com/collections/QueueExample.java
+++ Day11/src/com/collections/QueueExample.java
@@ -1,5 +1,33 @@
 package com.collections;
 
-public class QueueExample {
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+
+public class QueueExample {
+	public static void main(String[] args) {
+		Queue<String> qu = new PriorityQueue<>();
+		qu.add("sahil");
+		qu.add("khandelwal's");
+		qu.add("hello");
+		qu.add("world");
+		qu.add("into");
+		
+		
+		System.out.println(qu);
+		
+		Iterator<String> itr = qu.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+		System.out.println("-------------");
+		qu.poll();
+		qu.remove();
+		itr = qu.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
 }
+
